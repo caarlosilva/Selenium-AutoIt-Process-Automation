a6e7fce134508df9ee7ddd06c9a6287899777f45
Carlos Silva <caarlos.silva06@gmail.com>
2019-10-10 22:35:37 -0300

    created fb login first

--- MainForm/MainForm/Main.cs
+++ MainForm/MainForm/Main.cs
@@ -56,5 +56,49 @@ namespace MainForm
 
         }
 
+        // Facebook
+
+        // Facebook
+
+        public static void loginFacebook(string user, string pwd)
+        {
+            // set driver to open G. Chrome
+            driver = new ChromeDriver();
+            // set url
+            string url = "http://www.facebook.com";
+            // go to the url
+            driver.Navigate().GoToUrl(url);
+
+            // create 'wait' object to wait 15 seconds
+            WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
+            // maximize the browser
+            driver.Manage().Window.Maximize();
+            AutoItX.Sleep(1000);
+            // wait 10 seconds to find element LOGIN
+            wait.Until(driver => driver.FindElement(By.Name("email")));
+            // set element login
+            IWebElement elLogin = driver.FindElement(By.Name("email"));
+
+            // wait 10 seconds to find element LOGIN
+            wait.Until(driver => driver.FindElement(By.Name("pass")));
+            //set element password
+            IWebElement elPassword = driver.FindElement(By.Name("pass"));
+
+            // setting the field LOGIN ans PASSWORD to value of user parameter
+            elLogin.SendKeys(user);
+            elPassword.SendKeys(pwd);
+
+            // initiate a java script executor, so we can inject js to the page
+            IJavaScriptExecutor js = (IJavaScriptExecutor)driver;
+
+            // emulate a click on the button
+            js.ExecuteScript("var el = document.querySelector('[type=\"submit\"]');" +
+                "el.click()");
+
+            // if u want to close the driver
+            //driver.Close();
+
+        }
+
     }
 }
